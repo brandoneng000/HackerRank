87fb142c115d34c7504f60136183b55aa5d0179e
Brandon Eng <brandoneng000@gmail.com>
2017-03-16 18:20:25 -0700

    changed name

--- Test/src/test.java
+++ Test/src/test.java
@@ -4,6 +4,7 @@ public class test {
    public static void main(String[] args) {
       // TODO Auto-generated method stub
       System.out.println("This is a test!!");
+      System.out.println("New name");
    }
 
 }
